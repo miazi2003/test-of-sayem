ad7de6f26e596c82184290fff74e5018e05de13f
miazi2003 <yeasinmiazi1997@gmail.com>
2025-06-06 11:45:17 +0600

    again pushed

--- index.js
+++ index.js
@@ -61,6 +61,18 @@ async function run() {
       res.send(result)
     });
 
+
+
+
+
+     app.delete("/users/:id", async(req, res) => {
+      const id = req.params.id;
+      console.log("to be deleted : ", id);
+      const query ={_id : new ObjectId(id)}
+      const result = await userCollection.deleteOne(query)
+      res.send(result)
+    });
+
     await client.db("admin").command({ ping: 1 });
     console.log("pinged your deployment");
   } catch {}
